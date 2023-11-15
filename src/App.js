30419a81ab0f22a0c638808e812c064f1b77d85a
leonarjin <izumi.jin@live.com>
2023-11-15 22:05:28 +0000

    Teste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Axios from "axios";
+import { configure } from "axios-hooks";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Search } from "./screens/Search";
 import { Details } from "./screens/Details";
 import { NormalizeStyles } from "./shared/NormalizeStyles";
 import { Header } from "./common-components/Header/Header";
+const axios = Axios.create({
+  baseURL: `${process.env.REACT_APP_SUPER_HERO_API_BASE_URL}/${process.env.REACT_APP_SUPER_HERO_API_KEY}`,
+});
+configure({ axios });
 export function App() {
   return (
     <>
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,61 +1,101 @@
+import React from 'react';
 import styled from 'styled-components';
-import { Card } from '../common-components/Card/Card';
-import { Caption } from '../common-components/Caption/Caption';
-import { Description } from '../common-components/Description/Description';
+import { Alert } from '../common-components/Alert/Alert';
 import { Flex, Box } from 'reflexbox';
 import { Button } from '../common-components/Button/Button';
 import { SearchField } from '../common-components/SearchField/SearchField';
 import { HeroCard } from '../components/HeroCard/HeroCard';
+import { HeroCardLoader } from '../components/HeroCard/HeroCardLoader';
 import { Spaces } from '../shared/DesignTokens';
+import axios from 'axios';
+import useAxios from 'axios-hooks';
 const HeroesGrid = styled(Box)`
-    display: grid;
-    grid-template-column: 1fr;
-    gap: ${Spaces.ONE_HALF};
-    @media (min-width: 1024px) {
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        gap: ${Spaces.TWO};
-    }
-`
+	display: grid;
+	grid-template-columns: 1fr;
+	gap: ${Spaces.ONE_HALF};
+	@media (min-width: 1024px) {
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		gap: ${Spaces.TWO};
+	}
+`;
+async function searchHero(heroName) {
+	const { data } = await axios.get(`/search/${heroName}`, {
+		baseURL: `${process.env.REACT_APP_SUPER_HERO_API_BASE_URL}/${process.env.REACT_APP_SUPER_HERO_API_KEY}`,
+	});
+	return data.results || [];
+}
 export function Search() {
+
+	const [search, setSearch] = React.useState({
+		value: 'captain',
+		doSearch: false,
+	});
+	const [{ data: heroes, loading: isLoadingHeroes }, searchHero] = useAxios(
+		`/search/${search.value}`,
+		{ manual: true }
+	);
+	React.useEffect(() => {
+		searchHero();
+	}, []);
+	function handleUpdateSearchValue({ target: { value } }) {
+		setSearch((prevValue) => ({ ...prevValue, value }));
+	}
+	function handleSearch() {
+		setSearch((prevValue) => ({ ...prevValue, doSearch: true }));
+	}
 	return (
-        <>
-            <Flex 
-                width={['100%', '600px']}
-                mx={[Spaces.NONE, 'auto']}
-                px={[Spaces.THREE, Spaces.FIVE]}
-                px={[Spaces.ONE, Spaces.NONE]}
-                mb={[Spaces.TWO, Spaces.FOUR]}
-            >
-                <Box flexGrow="1">
-                    <SearchField placeholder="Digite um nome de herói ou heroína" />
-                </Box>
-                <Box ml={Spaces.TWO}>
-                    <Button>Buscar</Button>
-                </Box>
-            </Flex>
-            <HeroesGrid
-				px={[Spaces.ONE, Spaces.TWO]}
-				pb={[Spaces.ONE, Spaces.TWO]}
+		<>
+			<Flex
+				width={['100%', '600px']}
+				mx={[Spaces.None, 'auto']}
+				mt={[Spaces.THREE, Spaces.FIVE]}
+				px={[Spaces.ONE, Spaces.NONE]}
+				mb={[Spaces.TWO, Spaces.FOUR]}
 			>
-				<HeroCard
-					secretIdentity="Terry McGinnis"
-					name="Batman"
-					picture="https://www.superherodb.com/pictures2/portraits/10/100/10441.jpg"
-					universe="DC Comics"
-				/>
-				<HeroCard
-					secretIdentity="Bruce Wayne"
-					name="Batman"
-					picture="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
-					universe="DC Comics"
-				/>
-				<HeroCard
-					secretIdentity="Dick Grayson"
-					name="Batman II"
-					picture="https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg"
-					universe="DC Comics"
-				/>
-			</HeroesGrid>
-        </>
-    );
+				<Box flexGrow="1">
+					<SearchField
+						placeholder="Digite um nome de herói ou heroína"
+						onKeyUp={handleUpdateSearchValue}
+					/>
+				</Box>
+				<Box ml={Spaces.TWO}>
+					<Button onClick={handleSearch}>Buscar</Button>
+				</Box>
+			</Flex>
+			{!isLoadingHeroes && heroes && heroes.error ? (
+				<Box
+					px={[Spaces.ONE, Spaces.TWO]}
+					pb={[Spaces.ONE, Spaces.TWO]}
+				>
+					<Alert type="info">
+						Nenhum registro de herói ou heroína foi encontrado.
+					</Alert>
+				</Box>
+			) : (
+				<HeroesGrid
+					px={[Spaces.ONE, Spaces.TWO]}
+					pb={[Spaces.ONE, Spaces.TWO]}
+				>
+					{isLoadingHeroes && (
+						<>
+							<HeroCardLoader />
+							<HeroCardLoader />
+							<HeroCardLoader />
+							<HeroCardLoader />
+						</>
+					)}
+					{!isLoadingHeroes && heroes && heroes.results.map((hero) => (
+						<HeroCard
+							key={hero.id}
+							id={hero.id}
+							secretIdentity={hero.biography['full-name']}
+							name={hero.name}
+							picture={hero.image.url}
+							universe={hero.biography.publisher}
+						/>
+					))}
+				</HeroesGrid>
+			)}
+		</>
+	);
 }
