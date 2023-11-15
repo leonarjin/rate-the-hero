63735d6bec74373b514ec1580d149035da748222
leonarjin <izumi.jin@live.com>
2023-11-15 19:05:15 +0000

    feat: create HeroCard component and Search screen structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Search } from "./screens/Search";
 import { Details } from "./screens/Details";
+import { NormalizeStyles } from "./shared/NormalizeStyles";
+import { Header } from "./common-components/Header/Header";
 export function App() {
-    return (
+  return (
+    <>
+      <NormalizeStyles />
       <BrowserRouter>
+        <Header />
         <Routes>
-            <Route path="/detalhes/:id" element={<Details />} />
-            <Route path="/" element={<Search />} />
-            <Route path="*">Página não encontrada. </Route>
+          <Route path="/detalhes/:id" element={<Details />} />
+          <Route path="/" element={<Search />} />
+          <Route path="*">Página não encontrada. </Route>
         </Routes>
-    </BrowserRouter>
-    );
-  }
+      </BrowserRouter>
+    </>
+  );
+}
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,3 +1,61 @@
-export function Search(){
-    return <div>Hello, tela de busca!</div>;
+import styled from 'styled-components';
+import { Card } from '../common-components/Card/Card';
+import { Caption } from '../common-components/Caption/Caption';
+import { Description } from '../common-components/Description/Description';
+import { Flex, Box } from 'reflexbox';
+import { Button } from '../common-components/Button/Button';
+import { SearchField } from '../common-components/SearchField/SearchField';
+import { HeroCard } from '../components/HeroCard/HeroCard';
+import { Spaces } from '../shared/DesignTokens';
+const HeroesGrid = styled(Box)`
+    display: grid;
+    grid-template-column: 1fr;
+    gap: ${Spaces.ONE_HALF};
+    @media (min-width: 1024px) {
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        gap: ${Spaces.TWO};
+    }
+`
+export function Search() {
+	return (
+        <>
+            <Flex 
+                width={['100%', '600px']}
+                mx={[Spaces.NONE, 'auto']}
+                px={[Spaces.THREE, Spaces.FIVE]}
+                px={[Spaces.ONE, Spaces.NONE]}
+                mb={[Spaces.TWO, Spaces.FOUR]}
+            >
+                <Box flexGrow="1">
+                    <SearchField placeholder="Digite um nome de herói ou heroína" />
+                </Box>
+                <Box ml={Spaces.TWO}>
+                    <Button>Buscar</Button>
+                </Box>
+            </Flex>
+            <HeroesGrid
+				px={[Spaces.ONE, Spaces.TWO]}
+				pb={[Spaces.ONE, Spaces.TWO]}
+			>
+				<HeroCard
+					secretIdentity="Terry McGinnis"
+					name="Batman"
+					picture="https://www.superherodb.com/pictures2/portraits/10/100/10441.jpg"
+					universe="DC Comics"
+				/>
+				<HeroCard
+					secretIdentity="Bruce Wayne"
+					name="Batman"
+					picture="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+					universe="DC Comics"
+				/>
+				<HeroCard
+					secretIdentity="Dick Grayson"
+					name="Batman II"
+					picture="https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg"
+					universe="DC Comics"
+				/>
+			</HeroesGrid>
+        </>
+    );
 }
